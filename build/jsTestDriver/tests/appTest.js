d27a272ab75c681db0898ed6b6a3bed2b50676bb
Jinker Jiang <jinkerjiang@gmail.com>
2013-05-26 13:29:42 +0800

    closure modules generate jsTestDriver config file

--- build/jsTestDriver/tests/appTest.js
+++ build/jsTestDriver/tests/appTest.js
@@ -1,8 +1,35 @@
-appTest = TestCase("app");
+//modules:app
 
-appTest.prototype.testGreet = function () {
-	assertEquals("m1 hello", m1.hello());
-};
-appTest.prototype.testGreet1 = function () {
-	assertEquals("m2 hello", m2.hello());
-};
+TestCase("app", {
+	setUp: function () {
+		this.m1 = m1;
+		this.m2 = m2;
+	},
+	teardown: function () {
+		delete this.m1;
+		delete this.m2;
+	},
+	testGreet: function () {
+		assertEquals("m1 hello", this.m1.hello());
+	},
+	testGreet1: function () {
+		assertEquals("m2 hello", this.m2.hello());
+	}
+});
+
+TestCase("app1", {
+	setUp: function () {
+		this.m1 = m1;
+		this.m2 = m2;
+	},
+	teardown: function () {
+		delete this.m1;
+		delete this.m2;
+	},
+	testGreet: function () {
+		assertEquals("m1 hello", this.m1.hello());
+	},
+	testGreet1: function () {
+		assertEquals("m2 hello", this.m2.hello());
+	}
+});
